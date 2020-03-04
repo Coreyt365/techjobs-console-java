55f10c8634dde926514d5008eb004aa235df16d8
Coreyt365 <coritye@gmail.com>
2020-03-04 08:43:21 -0600

    completed assignment 1

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -57,12 +58,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -81,6 +82,24 @@ public class JobData {
             }
         }
 
+        return jobs;
+    }
+    public static ArrayList<HashMap<String, String>> findByValue (String value) {
+
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+
+        for (HashMap<String, String> row : allJobs) {
+
+       for(Map.Entry<String,String> entry: row.entrySet()){
+           if(entry.getValue().toLowerCase().contains(value.toLowerCase())){
+               jobs.add(row);
+           }
+       }
+        }
         return jobs;
     }
 
@@ -124,5 +143,4 @@ public class JobData {
             e.printStackTrace();
         }
     }
-
 }
